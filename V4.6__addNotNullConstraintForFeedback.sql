58243e6a2f6414d83e94eef257062f19efcf7865
Nikita Lobanov <114010651+NiLomy@users.noreply.github.com>
2023-11-02 20:19:20 +0300

    Update V4.6__addNotNullConstraintForFeedback.sql

--- V4.6__addNotNullConstraintForFeedback.sql
+++ V4.6__addNotNullConstraintForFeedback.sql
@@ -1,6 +1,6 @@
 alter table feedback
 	alter column person_id set not null,
-	alter column "role" set not null,
+	alter column role_id set not null,
 	alter column hackathon_id set not null,
 	alter column comment_text set not null,
 	alter column raiting set not null;
