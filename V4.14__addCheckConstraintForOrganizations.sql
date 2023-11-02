7ebcfa65a98c7ee39e1f1276f3dc348f0832ed85
Nikita Lobanov <114010651+NiLomy@users.noreply.github.com>
2023-11-02 19:38:59 +0300

    Add check constraints for Hackathons Organizations

---
+++ V4.14__addCheckConstraintForOrganizations.sql
@@ -0,0 +1,5 @@
+alter table organizations
+ 	add constraint imageurlchk check (image_url ~* '^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$'),
+ 	add constraint emailchk check (email ~* '^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$'),
+ 	add constraint phonechk check (phone_number ~* '^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$'),
+ 	add constraint websiteurlchk check (website_url ~* '^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$');
