650b7baf4496cbe301c6fd3fc14f0c733cdb7db7
Nikita Lobanov <114010651+NiLomy@users.noreply.github.com>
2023-11-02 20:20:06 +0300

    Update V4.9__addForeignKeyConstraintForFeedback.sql

--- V4.9__addForeignKeyConstraintForFeedback.sql
+++ V4.9__addForeignKeyConstraintForFeedback.sql
@@ -1,4 +1,4 @@
 alter table feedback
 	add constraint personfk foreign key (person_id) references persons (uuid),
-	add constraint rolefk foreign key ("role") references FeedbackRoles (role_id),
+	add constraint rolefk foreign key (role_id) references FeedbackRoles (role_id),
 	add constraint hackathonfk foreign key (hackathon_id) references hackathons (hackathon_id);
